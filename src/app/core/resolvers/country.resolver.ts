19394cb2285758e91b9a23dcc0323e5ba5481617
annaswitaj <annaswitaj.sw@gmail.com>
2023-03-15 18:16:06 +0100

    fix: regions resolver by consts

--- src/app/core/resolvers/country.resolver.ts
+++ src/app/core/resolvers/country.resolver.ts
@@ -1,26 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Router, Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { catchError, EMPTY, Observable } from 'rxjs';
-import { CountryDetails } from '../models/country.model';
-import { CountriesService } from '../sevices/countries.service';
+import { Resolve } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { regions } from '../consts/regions';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CountryResolver implements Resolve<CountryDetails> {
-  constructor(
-    private countriesService: CountriesService,
-    private router: Router
-  ) {}
-
-  resolve(route: ActivatedRouteSnapshot): Observable<CountryDetails> {
-    return this.countriesService
-      .getCountryDetailsByCountryCode(route.params?.['countryCode'])
-      .pipe(
-        catchError(() => {
-          this.router.navigate([route.params?.['countryCode']]);
-          return EMPTY;
-        })
-      ) as Observable<CountryDetails>;
+export class CountryResolver implements Resolve<Observable<typeof regions>> {
+  resolve(): Observable<typeof regions> {
+    return of(regions);
   }
 }
